ad5403ceb8d7ed027a0abb1f58a861601deac93d
adib-commits <hdadib442@gmail.com>
2026-05-07 11:17:04 +0200

    Create an executable file named structured_output.py that prints exactly data

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -6,6 +6,6 @@ pi_value = 3.14159
 computation_valid = (2 + 2 == 4)
 
 print("Language: {}".format(language))
-print(f"Version: {}".format(language))
-print(f"Pi approx: {}".format(language))
-print(f"Computation valid: {}".format(language))
+print("Version: {}".format(language))
+print("Pi approx: {:.2f}".format(language))
+print("Computation valid: {}".format(language))
