5273126707506ab701a2565b87e7e6bac3bae6d8
adib-commits <hdadib442@gmail.com>
2026-05-07 09:50:41 +0200

    Delete python_fundamentals/control_flow directory

--- python_fundamentals/control_flow/print_comb3.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        print("{}{}" .format(i, j), end=", " if i != 8 or j != 9 else "\n")
--- python_fundamentals/control_flow/print_hexa.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-for i in range(0, 99):
-    print("{} = 0x{:x}".format(i, i))
