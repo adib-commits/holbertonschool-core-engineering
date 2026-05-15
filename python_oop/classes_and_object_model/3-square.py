ffbd3350a0483dd0e8a3bd4ea687a49695985eac
adib-commits <hdadib442@gmail.com>
2026-05-15 11:30:29 +0200

    That returns the area of the square based on it's side length

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module that defines a Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a square with a validated private size attribute.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Return the current square area.
+
+        Returns:
+            int: Area of the square.
+        """
+
+        return self.__size ** 2
