377f35ac28200e87afbd878d610000d3bedd0ad8
adib-commits <hdadib442@gmail.com>
2026-05-15 11:28:34 +0200

    Ensure that object state is valid.

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module that defines a Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a square with a validated private size attribute.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
