0fe15b8f7530a2699c15a3f0867cee72ed0a8680
adib-commits <hdadib442@gmail.com>
2026-05-17 11:32:05 +0200

    That divides two integers

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""Module that divides two integers safely."""
-
-
 def safe_print_division(a, b):
-    """Divide two integers and print the result."""
     result = None
+
     try:
         result = a / b
     except Exception:
-        pass
+        result = None
     finally:
         print("Inside result: {}".format(result))
+
     return result
