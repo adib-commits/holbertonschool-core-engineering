ebf1cbd92701cceacca0e04bd93ffa70f030c6d2
adib-commits <hdadib442@gmail.com>
2026-05-25 11:16:24 +0200

    That reads a text file (UTF8) and prints it to stdout

---
+++ python_advanced/file_handling/read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Module read_file"""
+
+def read_file(filename=""):
+    """Read a UTF8 text file and print it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
