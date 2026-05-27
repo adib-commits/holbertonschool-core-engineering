7bedb8771ffaf2348e738755e7d0b3cb6ebff343
adib-commits <hdadib442@gmail.com>
2026-05-27 14:37:54 +0200

    That writes a string to a text file (UTF-8) and returns the number of characters written

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Module for writing to a file"""
+
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file and return number of chars"""
+    """Writes a string to a UTF-8 text file and returns
+    the number of characters written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
