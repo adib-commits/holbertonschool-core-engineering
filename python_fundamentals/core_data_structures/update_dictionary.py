aed8cc8c1df86dcca253f3a22625a86d476f3691
adib-commits <hdadib442@gmail.com>
2026-06-23 14:42:33 +0000

    That replaces or adds a key/value pair in a dictionary

---
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Update or add a key/value in a dictionary"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add a key/value pair"""
+    a_dictionary[key] = value
+    return a_dictionary
