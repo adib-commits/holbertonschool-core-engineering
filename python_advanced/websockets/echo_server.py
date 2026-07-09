3f64a41c7bc6353f60001b4f3f0b69aa7d32a447
adib-commits <hdadib442@gmail.com>
2026-07-09 14:01:16 +0200

    That creates a Websocket server

---
+++ python_advanced/websockets/echo_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+
+
+async def connection_handler(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+async def main():
+    async with websockets.serve(connection_handler, "localhost", 8765):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
